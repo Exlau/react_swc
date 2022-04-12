503a8d590395d96e587010f9913db133f80bed3c
Exlau <285874446@qq.com>
2022-04-12 23:44:26 +0800

    1.swc打包

--- out.js
+++
@@ -1,9 +0,0 @@
-console.log(process.env.SWC_ENV);
-console.log(a);
-
-var a = function() {
-    return "a";
-};
-
-
-//# sourceMappingURL=out.js.map
--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ const func = ()=>{
     console.log("INDEX")
     x();
 }
+
+func();
